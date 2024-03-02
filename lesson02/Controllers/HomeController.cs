7a6f6bc1a4ad463c02eac33457c1ba4d80222fa1
Arya <144851489+Aryahaseli@users.noreply.github.com>
2024-03-03 02:26:22 +0300

    Add files via upload

---
+++ lesson02/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using lesson02.Models;
+
+namespace lesson02.Controllers;
+
+public class HomeController : Controller
+{
+    public IActionResult Index()
+    {
+        Random rnd = new Random();
+        ViewBag.number = rnd.Next(0, 100);
+        return View();
+    }
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+    public IActionResult Contact()
+    {
+        ViewData["Name"] = "Barış Gürkan";
+        ViewBag.ders = "Web Tasarım ve Programlama";
+        return View();
+    }
+    public IActionResult Lessons()
+    {
+        string[] lessonList = {
+            "Web Tabanlı Uygulama Geliştirme",
+            "Grafik ve Canlandırma",
+            "Oyun Programlama",
+            "Mobil Uygulama Geliştirme",
+            "Web Tabanlı İçerik Yönetimi",
+            "Matematik",
+            "Türkçe",
+            "Beden Eğitimi"
+        };
+
+        ViewBag.lessons = lessonList;
+
+        return View();
+    }
+
+    public IActionResult Projects()
+    {
+        return View();
+    }
+}
---
+++ lesson02/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace lesson02.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
